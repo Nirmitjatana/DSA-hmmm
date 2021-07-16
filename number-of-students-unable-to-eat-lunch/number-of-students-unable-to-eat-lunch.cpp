af4ad3734ee0cca243f667a5900fb709e2d54868
Nox_codes <52108077+Nirmitjatana@users.noreply.github.com>
2021-07-16 13:34:21 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (20.97%) - LeetHub

--- number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -2,24 +2,53 @@ class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sw) {
         queue<int> q;
-        int n = students.size();
-        for(int i=0; i<n; i++) {
+        stack<int> s;
+        
+        for(int i=sw.size()-1;i>-1;i--){
+            s.push(sw[i]);
+        }
+        for(int i=0;i<students.size();i++){
             q.push(students[i]);
         }
-        int i=0;
         int count = 0;
+        int n = students.size();
         while(true) {
             if(q.empty() || count > 5*n) break;
-            if(q.front() == sw[i]) {
+            if(s.top() == q.front()){
+                s.pop();
                 q.pop();
-                sw.erase(sw.begin());
-            } else {
-                int x = q.front();
+            }
+            else{
+                int back = q.front();
                 q.pop();
-                q.push(x);
+                q.push(back);
             }
             count++;
         }
         return q.size();
+        
+        
+        
+        
+        
+        // int n = students.size();
+        // for(int i=0; i<n; i++) {
+        //     q.push(students[i]);
+        // }
+        // int i=0;
+        // int count = 0;
+        // while(true) {
+        //     if(q.empty() || count > 5*n) break;
+        //     if(q.front() == sw[i]) {
+        //         q.pop();
+        //         sw.erase(sw.begin());
+        //     } else {
+        //         int x = q.front();
+        //         q.pop();
+        //         q.push(x);
+        //     }
+        //     count++;
+        // }
+        // return q.size();
     }
 };
